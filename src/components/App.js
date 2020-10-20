ae2a122bac5d6ee0dcd4c782ac61343bd19fc536
NightRider0098 <39752890+Nightrider0098@users.noreply.github.com>
2020-10-20 17:22:27 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,25 @@
 import React, { Component, useState } from "react";
-import '../styles/App.css';
+import "../styles/App.css";
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,
-            x : 0,
-			y: 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderChoice.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
-		this.reset = this.reset.bind(this)
-    };
+const App = () => {
+  const [renderBall, setRenderBall] = useState(false);
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [ballPosition,setBallPosition] = useState({
+    left: "0px",
+    top: "0px",
+  });
+  const reset = () => {};
+  const renderChoice = () => {};
 
-reset(){
-	
-}
-    buttonClickHandler() {
-    
-    }
-    renderChoice() {
-        if (this.state.renderBall === false) {
-            return <button onClick={this.buttonClickHandler} className="ballProvider" >Make a Ball</button>
-        }
-        else if (this.state.renderBall === true) {
-            return <div className="ball" style={this.state.ballPosition}></div>
-        }
-    }
-
-    componentDidMount() {
-        
-	
-	}
-
-    render() {
-        return (
-            <div className="playground">
-			<button onClick={this.reset} className="reset">Reset</button>
-                {this.renderChoice()}
-            </div>
-			
-        )
-    }
-}
-
+  return (
+    <div className="playground">
+      <button onClick={reset} className="reset">
+        Reset
+      </button>
+      {renderChoice()}
+    </div>
+  );
+};
 
 export default App;
